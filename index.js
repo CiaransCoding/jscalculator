4c0b57ad29118405011a664f92f37edcd688e5a8
Ciaran <ciaran_corrigan@hotmail.com>
2021-03-13 23:37:20 +0000

    fixed it

--- index.js
+++ index.js
@@ -12,8 +12,29 @@ var operators = [add, subtract, multiply, divide];
 document.querySelector("#calculate").addEventListener("click", () => {
     var num1 = parseInt(document.querySelector("#num1").value);
     var num2 = parseInt(document.querySelector("#num2").value);
+    var answer = document.querySelector("#answer");
+    var func;
 
-    document.querySelector("#answer").value = calculate(num1, num2, operators[0]);
+    switch (operator) {
+        case "add":
+            func = add;
+            break;
+        case "subtract":
+            func = subtract;
+            break;
+        case "multiply":
+            func = multiply;
+            break;
+        case "divide":
+            func = divide;
+            break;
+        default:
+            break;
+    };
+
+    console.log(func);
+
+    answer.value = calculate(num1, num2, func);
 });
 
 function add(num1, num2) {
@@ -21,8 +42,8 @@ function add(num1, num2) {
 };
 
 function subtract(num1, num2) {
-    return num1 = num2;
-}
+    return num1 - num2;
+};
 
 function multiply(num1, num2) {
     return num1 * num2;
@@ -30,7 +51,7 @@ function multiply(num1, num2) {
 
 function divide(num1, num2) {
     return num1 / num2;;
-}
+};
 
 function calculate(num1, num2, operator) {
     return operator(num1, num2);
